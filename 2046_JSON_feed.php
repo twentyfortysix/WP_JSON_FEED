3c0554521c8272d69b66338ef3bb240ab11b6ca9
twentyfortysix <2046@2046.cz>
2017-11-02 16:02:24 +0100

    simplified to json_decode

--- 2046_JSON_feed.php
+++ 2046_JSON_feed.php
@@ -4,58 +4,38 @@ Plugin Name: 2046 JSON feed
 Plugin URI: http://2046.cz
 Description: Extra feed in JSON format.
 Author: 2046
-Version: 0.1
+Version: 0.2
 Author URI: http://2046.cz
 */
 
- //  add url variable
-// function f2046_add_json_feed_variable( $qvars ){
-// 	$qvars[] = 'json_feed_var';
-// 	return $qvars;
-// }
-// add_filter('query_vars', 'f2046_add_json_feed_variable' );
-
 // get the subtitles for a post with given custom_meta
 function f2046_get_json_feed() {
-	header('Content-Type: text/html; charset=utf-8');
-	global $post, $wp_query;
-	// mydump($wp_query->posts);
-	// clean the variable 
-	$output = '';
-	$title = '';
-	$link = '';
-	$image = array();
-	$excerpt = '';
+	header('Content-Type: text/html; charset=utf-8'); // <-- just for previe
+	// header('Content-Type: application/json'); // <-- run if you wanna get the JSON proper header
+	$output = array();
 
-	
 	// The Query
 	if (have_posts()) :	
-		$output = "{\n";
 		// The Loop
+		$i = 0;
 		while ( have_posts() ) : 
 				the_post();
-				$image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium');
 				$excerpt = strip_tags(get_the_excerpt());
 				$title = get_the_title();
 				$link = get_permalink();
 				
-				$output .= '
-	"'.$post->ID.'" : {
-		"title" : "'.$title.'",
-		"link" : "'.$link.'",
-		"image" : "'.$image[0].'",
-		"excerpt" : "'.$excerpt.'"
-	}';
-				if($post != end($wp_query->posts)){
-					$output .= ",\n";
-				}
+				$output[$i] = array(
+					"id" => $post->ID,
+					"title" => $title,
+					"url" => $link,
+					"excerpt" => $excerpt,
+				);
+			$i++;
 		endwhile;
-		$output .= "\n}";
+	
 	endif;
-	echo $output;
-	// Restore original Query & Post Data
-	wp_reset_query();
-	wp_reset_postdata();
+	echo json_decode($output);
+	
 }
 
 // ignite feed where we can get the function 	result
